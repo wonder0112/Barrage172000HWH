30c60ac483f5247f88c268750d43c04f324e38fe
wonder0112 <hwhgdcn@gmail.com>
2019-03-01 09:38:44 +0800

    视频播放 播放视频和设置为循环播放

--- app/src/main/java/cn/edu/gdpt/xxgcx/barrage172000hwh/MainActivity.java
+++ app/src/main/java/cn/edu/gdpt/xxgcx/barrage172000hwh/MainActivity.java
@@ -1,13 +1,81 @@
 package cn.edu.gdpt.xxgcx.barrage172000hwh;
 
-import android.support.v7.app.AppCompatActivity;
+import android.media.MediaPlayer;
+import android.net.Uri;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.text.TextUtils;
+import android.view.View;
+import android.widget.*;
 
-public class MainActivity extends AppCompatActivity {
+import master.flame.danmaku.ui.widget.DanmakuView;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
+    private VideoView mVdoViMainVideo;
+    private DanmakuView mKuViewMainBarrage;
+    private EditText mEdtTxtMainInput;
+    private Button mBtnMainSend;
+    private LinearLayout mLinLayMainBottom;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        initView();
+        playVideo();
+    }
+    /**
+     * 播放视频
+     */
+    private  void playVideo(){
+        String uri="android.resource://"+getPackageName()+"/"+R.raw.sun;//视频路径
+        if(uri!=null){
+            mVdoViMainVideo.setVideoURI(Uri.parse(uri));//视频赋给VideoView控件
+            mVdoViMainVideo.start();//播放
+            //循环播放
+            mVdoViMainVideo.setOnPreparedListener(new MediaPlayer.OnPreparedListener() {
+                @Override
+                public void onPrepared(MediaPlayer mp) {
+                    mp.start();
+                    mp.setLooping(true);
+                }
+            });
+        }else {
+            mVdoViMainVideo.getBackground().setAlpha(0);//设置背景透明
+        }
+
+    }
+
+    private void initView() {
+        mVdoViMainVideo = (VideoView) findViewById(R.id.vdoVi_main_video);
+        mKuViewMainBarrage = (DanmakuView) findViewById(R.id.kuView_main_barrage);
+        mEdtTxtMainInput = (EditText) findViewById(R.id.edtTxt_main_input);
+        mBtnMainSend = (Button) findViewById(R.id.btn_main_send);
+        mLinLayMainBottom = (LinearLayout) findViewById(R.id.linLay_main_bottom);
+
+        mBtnMainSend.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.btn_main_send:
+
+                break;
+        }
+    }
+
+    private void submit() {
+        // validate
+        String input = mEdtTxtMainInput.getText().toString().trim();
+        if (TextUtils.isEmpty(input)) {
+            Toast.makeText(this, "input不能为空", Toast.LENGTH_SHORT).show();
+            return;
+        }
+
+        // TODO validate success, do something
+
+
     }
 }
